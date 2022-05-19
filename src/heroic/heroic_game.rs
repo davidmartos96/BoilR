163c0641694f8d55c164ae7e4c36b347f834bc6a
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-05-19 13:10:10 +0200

    Can parse extened info

--- src/heroic/heroic_game.rs
+++ src/heroic/heroic_game.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::path::Path;
 
 use serde::{Deserialize, Serialize};
@@ -11,6 +12,104 @@ pub struct HeroicGame {
     pub install_path: String,
     pub executable: String,
     pub launch_parameters: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct HeroicGameExtended {
+    #[serde(rename = "audioFix")]
+    audio_fix: Option<bool>,
+
+    #[serde(rename = "autoSyncSaves")]
+    auto_sync_saves: Option<bool>,
+
+    #[serde(rename = "savesPath")]
+    saves_path: Option<String>,
+
+    #[serde(rename = "enableEsync")]
+    enable_esync: Option<bool>,
+
+    #[serde(rename = "enableFsync")]
+    enable_fsync: Option<bool>,
+
+    #[serde(rename = "enableFSR")]
+    enable_fsr: Option<bool>,
+
+    #[serde(rename = "maxSharpness")]
+    max_sharpness: Option<bool>,
+
+    #[serde(rename = "enableResizableBar")]
+    enable_resizable_bar: Option<bool>,
+
+    #[serde(rename = "nvidiaPrime")]
+    nvidia_prime: Option<bool>,
+
+    #[serde(rename = "offlineMode")]
+    offline_mode: Option<bool>,
+
+    #[serde(rename = "showFps")]
+    show_fps: Option<bool>,
+
+    #[serde(rename = "showMangehud")]
+    show_mangehud: Option<bool>,
+
+    #[serde(rename = "useGameMode")]
+    use_game_mode: Option<bool>,
+
+    #[serde(rename = "launcherArgs")]
+    launcher_args: Option<String>,
+
+    #[serde(rename = "otherOptions")]
+    other_options: Option<String>,
+
+    #[serde(rename = "targetExe")]
+    target_exe: Option<String>,
+
+    #[serde(rename = "useSteamRuntime")]
+    use_steam_runtime: Option<bool>,
+
+    #[serde(rename = "winePrefix")]
+    wine_prefix: Option<String>,
+
+    #[serde(rename = "wineVersion")]
+    wine_version: Option<WineVersion>,
+
+    #[serde(rename = "altLegendaryBin")]
+    alt_legendary_bin: Option<String>,
+
+    #[serde(rename = "altGogdlBin")]
+    alt_gogdl_bin: Option<String>,
+
+    #[serde(rename = "egsLinkedPath")]
+    egs_linked_path: Option<String>,
+
+    #[serde(rename = "maxRecentGames")]
+    max_recent_games: Option<usize>,
+
+    #[serde(rename = "checkUpdateInterval")]
+    check_update_interval: Option<usize>,
+
+    #[serde(rename = "enable_updates")]
+    enable_udates: Option<bool>,
+
+    #[serde(rename = "appName")]
+    app_name: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct WineVersion {
+    bin: Option<String>,
+    name: Option<String>,
+
+    #[serde(rename = "type")]
+    wine_type: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+enum HeroicExtendedFields {
+    Game(HeroicGameExtended),
+    String(String),
+    Bool(bool),
 }
 
 impl HeroicGame {
@@ -42,11 +141,6 @@ impl From<HeroicGame> for ShortcutOwned {
                 install_path = format!("\"{}\"", install_path);
             }
         }
-        #[cfg(target_os = "windows")]
-        let install_path = game.install_path.to_string();
-
-        #[cfg(target_os = "windows")]
-        let target = target_path.to_string_lossy().to_string();
 
         let shortcut = Shortcut::new(
             "0",
@@ -63,5 +157,41 @@ impl From<HeroicGame> for ShortcutOwned {
         owned_shortcut.tags.push("Installed".to_owned());
 
         owned_shortcut
+    }
+}
+
+pub fn parse_json(json: &str) -> Option<HeroicGameExtended> {
+    let json_result = serde_json::from_str::<HashMap<String, HeroicExtendedFields>>(json);
+    if !json_result.is_ok() {
+        println!("{:?}", json_result);
+        return None;
+    }
+    let game_map = json_result.unwrap();
+    for (key, value) in game_map.iter() {
+        match value {
+            HeroicExtendedFields::Game(val) => {
+                let mut res = val.clone();
+                res.app_name = Some(key.clone());
+                return Some(res);
+            }
+            HeroicExtendedFields::String(_) => {}
+            HeroicExtendedFields::Bool(_) => {}
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_can_parse_extended() {
+        let json = include_str!("../testdata/heroic_extened_game.json");
+        let game = parse_json(json);
+        let game = game.unwrap();
+        let app_name = "9b40e3ffb4074f22a856a521be5ce858";
+        assert_eq!(app_name, game.app_name.unwrap().as_str());
+        assert_eq!(false, game.nvidia_prime.unwrap());
     }
 }
